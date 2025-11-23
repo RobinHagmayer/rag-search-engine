bd64f84bc0b634d1df02a079a46300a12885f173
Robin Hagmayer <robin.hagmayer@proton.me>
2025-11-23 14:27:37 +0100

    added inverted index for optimized searching

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,21 +1,93 @@
+import pickle
 import string
+import sys
+from collections import defaultdict
 
 from nltk.stem import PorterStemmer
 
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
+from .search_utils import CACHE_DIR, DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
+
+
+class InvertedIndexLoadError(Exception):
+    pass
+
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.index = defaultdict(set)
+        self.docmap: dict[int, dict] = {}
+        self.index_path = CACHE_DIR / "index.pkl"
+        self.docmap_path = CACHE_DIR / "docmap.pkl"
+
+    def build(self) -> None:
+        movie_list = load_movies()
+        for movie in movie_list:
+            doc_id = movie["id"]
+            doc_text = f"{movie['title']} {movie['description']}"
+            self.docmap[doc_id] = movie
+            self.__add_document(doc_id, doc_text)
+
+    def save(self) -> None:
+        CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+        with self.index_path.open("wb") as f:
+            pickle.dump(self.index, f)
+
+        with self.docmap_path.open("wb") as f:
+            pickle.dump(self.docmap, f)
+
+    def get_documents(self, term: str) -> list[int]:
+        return sorted(self.index.get(term, set()))
+
+    def __add_document(self, doc_id: int, text: str) -> None:
+        token_list = tokenize_text(text)
+        for token in token_list:
+            self.index[token].add(doc_id)
+
+    def load(self) -> None:
+        try:
+            with self.index_path.open("rb") as f:
+                self.index = pickle.load(f)  # noqa: S301
+        except FileNotFoundError as e:
+            msg = f"Index file not found: {self.index_path}"
+            raise InvertedIndexLoadError(msg) from e
+
+        try:
+            with self.docmap_path.open("rb") as f:
+                self.docmap = pickle.load(f)  # noqa: S301
+        except FileNotFoundError as e:
+            msg = f"Docmap file not found: {self.docmap_path}"
+            raise InvertedIndexLoadError(msg) from e
+
+
+def build_command() -> None:
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    movies = load_movies()
-    results = []
+    idx = InvertedIndex()
 
-    for movie in movies:
-        query_tokens = tokenize_text(query)
-        title_tokens = tokenize_text(movie["title"])
-        if has_matching_token(query_tokens, title_tokens):
-            results.append(movie)
-            if len(results) >= limit:
+    try:
+        idx.load()
+    except InvertedIndexLoadError as e:
+        sys.exit(f"Error loading the index: {e}")
+
+    query_token_list = tokenize_text(query)
+    results = []
+    done = False
+    for token in query_token_list:
+        doc_id_list = idx.get_documents(token)
+
+        for doc_id in doc_id_list:
+            if len(results) == limit:
+                done = True
                 break
+            results.append(idx.docmap.get(doc_id))
+
+        if done:
+            break
 
     return results
 
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -4,15 +4,16 @@ from pathlib import Path
 DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
-DATA_PATH = PROJECT_ROOT / "data"
+DATA_DIR = PROJECT_ROOT / "data"
+CACHE_DIR = PROJECT_ROOT / "cache"
 
 
 def load_movies() -> list[dict]:
-    with Path(DATA_PATH / "movies.json").open() as file:
+    with Path(DATA_DIR / "movies.json").open() as file:
         data = json.load(file)
     return data["movies"]
 
 
 def load_stopwords() -> list[str]:
-    with Path(DATA_PATH / "stopwords.txt").open() as file:
+    with Path(DATA_DIR / "stopwords.txt").open() as file:
         return file.read().splitlines()
